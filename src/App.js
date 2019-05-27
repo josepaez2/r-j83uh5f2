9453366d8fd8ec14404fa5202ca894f8c79e6ddb
josepaez <josepaez_2@hotmail.com>
2019-05-27 16:51:04 -0500

    Adds solution

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    texto: "",
+    counter: 0
+  }
+  handler = ({target}) => {
+    const {name, value} = target
+    // console.log(name)
+    // console.log(value)
+    // console.log(value.length)
+    this.setState({counter: value.length})
+
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea name="texto" rows="3" onChange={this.handler}></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
